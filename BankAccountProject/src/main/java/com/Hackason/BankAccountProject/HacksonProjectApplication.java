9ca1f71b3061b9bf48dc0494b679b5aff0f0b801
crystal <2511275284@qq.com>
2025-06-08 11:24:51 +0800

    init project

---
+++ BankAccountProject/src/main/java/com/Hackason/BankAccountProject/HacksonProjectApplication.java
@@ -0,0 +1,12 @@
+package com.Hackason.BankAccountProject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackasonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackasonProjectApplication.class, args);
+	}
+
+}
