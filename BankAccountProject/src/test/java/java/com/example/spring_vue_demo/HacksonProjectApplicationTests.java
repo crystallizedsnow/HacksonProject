3123747783b500c7ed5017d540dfbbe63569cc20
crystal <2511275284@qq.com>
2025-06-08 11:13:10 +0800

    init project

---
+++ BankAccountProject/src/test/java/java/com/example/spring_vue_demo/HacksonProjectApplicationTests.java
@@ -0,0 +1,9 @@
+package java.com.example.spring_vue_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacksonProjectApplicationTests {
+
+}
